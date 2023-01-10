0e9fea0eb947bc48c9bff64bd33b92995fb09e0d
Jake <ozborn97@gmail.com>
2023-01-10 15:40:00 -0500

    Finished

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+//const { addDestinationInfo, pickPlanet } = require("./scriptHelper");
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
-});
+//const { myFetch } = require("./scriptHelper");
+
+window.addEventListener("load", function () {
+    let subButt = document.querySelector("form")
+    subButt.addEventListener("submit", function (event) {
+        event.preventDefault()
+         let pilotName = document.querySelector("input[name=pilotName]");
+         let copilotName = document.querySelector("input[name=copilotName]");
+         let fuelLevel = document.querySelector("input[name=fuelLevel]");
+         let cargoMass = document.querySelector("input[name=cargoMass]");
+         formSubmission(document, null, pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value)
+    console.log(event)
+    })
+    let listedPlanets;
+    //  Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        pickPlanet(listedPlanets)
+        
+        return result
+    }).then(function (listedPlanets) {  
+        console.log(listedPlanets);
+        
+        //Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+
+
+
+})
+})
